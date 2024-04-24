c7a33343eba44a7cad85749b4080ae5a2512c9d4
doonyango <datieno001@gmail.com>
2024-04-24 19:10:59 +0300

    update alphatext

--- alphatext.go
+++ alphatext.go
@@ -1,6 +1,7 @@
 package reload
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -10,13 +11,17 @@ func Capital(str []string) []string {
 	for i := 0; i < len(str); i++ {
 		if strings.Contains(str[i], "cap") {
 			if strings.Contains(str[i], "cap,") {
-				num, _ := strconv.Atoi(str[i+1][:len(str[i+1])-1])
+				num, err := strconv.Atoi(str[i+1][:len(str[i+1])-1])
+				if err != nil || num > len(str) || num < 0 {
+					fmt.Println("This is an error")
+					continue
+				}
 				for j := i - num; j < i; j++ {
-					str[j] = strings.Title(str[j])
+					str[j] = strings.ToUpper(string(str[j][0])) + strings.ToLower(string(str[j][1:]))
 				}
 				str = append(str[:i], str[i+2:]...)
 			} else {
-				str[i-1] = strings.Title(str[i-1])
+				str[i-1] = strings.ToUpper(string(str[i-1][0])) + strings.ToLower(string(str[i-1][1:]))
 				str = append(str[:i], str[i+1:]...)
 			}
 		}
@@ -27,9 +32,13 @@ func Capital(str []string) []string {
 // checks for any instances of up and up, and changes the previous slice of string to uppercase and removes the up and up,.
 func UpperCase(str []string) []string {
 	for i := 0; i < len(str); i++ {
-		if strings.Contains(str[i], "up") {
-			if strings.Contains(str[i], "up,") {
-				num, _ := strconv.Atoi(str[i+1][:len(str[i+1])-1])
+		if strings.Contains(str[i], "(up") {
+			if strings.Contains(str[i], "(up,") {
+				num, err := strconv.Atoi(str[i+1][:len(str[i+1])-1])
+				if err != nil || num > len(str) || num < 0 {
+					fmt.Println("This is an error")
+					continue
+				}
 				for k := i - num; k < i; k++ {
 					str[k] = strings.ToUpper(str[k])
 				}
@@ -46,9 +55,13 @@ func UpperCase(str []string) []string {
 // checks for instance of low and low, and changes the previous word to lowercase then removes the low and low,.
 func LowerCase(str []string) []string {
 	for i := 0; i < len(str); i++ {
-		if strings.Contains(str[i], "low") {
-			if strings.Contains(str[i], "low,") {
-				num, _ := strconv.Atoi(str[i+1][:len(str[i+1])-1])
+		if strings.Contains(str[i], "(low") {
+			if strings.Contains(str[i], "(low,") {
+				num, err := strconv.Atoi(str[i+1][:len(str[i+1])-1])
+				if err != nil || num > len(str) || num < 0 {
+					fmt.Println("this is an error")
+					continue
+				}
 				for j := i - num; j < i; j++ {
 					str[j] = strings.ToLower(str[j])
 				}
